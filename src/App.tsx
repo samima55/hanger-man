04366f0c0158f3dba7dadda90155224ace428aec
Samima Hassan <samimahassan55@gmail.com>
2023-10-31 12:55:41 +0100

    issue of double rending fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import words from "./assest/wordList.json";
 function App() {
+  const [wordToGuess , setWordToGuess]=useState(()=>{
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  console.log(wordToGuess)
+
   return (
-    <div>
-      hi
-      
-    </div>
+   <div></div>
   );
 }
 
