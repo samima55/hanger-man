5783473411058c0fa3721121e0a9ddc502150c4b
Samima Hassan <samimahassan55@gmail.com>
2023-11-02 21:54:52 +0100

    head added

--- src/copmponents/HangmanDrawing.tsx
+++ src/copmponents/HangmanDrawing.tsx
@@ -1,8 +1,16 @@
 import style from "./HangmanDrawing.module.css";
 
+const HEAD =(
+  <div className={style.head}>
+
+  </div>
+)
+
+
 function HangmanDrawing() {
   return (
     <div className={style.container}>
+      {HEAD}
       <div className={style.hangerMouth} />
       <div className={style.hangerWood} />
       <div className={style.hanger} />
