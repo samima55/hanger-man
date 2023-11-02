9bc4f0a8521fce3f6dc724ded822e08cebef4253
Samima Hassan <samimahassan55@gmail.com>
2023-11-02 22:14:05 +0100

    arm of hangman is added

--- src/copmponents/HangmanDrawing.tsx
+++ src/copmponents/HangmanDrawing.tsx
@@ -1,21 +1,20 @@
 import style from "./HangmanDrawing.module.css";
 
-const HEAD =(
-  <div className={style.head}>
-
-  </div>
-)
-
-const BODY =(
-  <div className={style.body}>
-
-  </div>
-)
+const HEAD = <div className={style.head}></div>;
+const BODY = <div className={style.body}></div>;
+const RIGHT_ARM = <div className={style.rightArm}></div>;
+const LEFT_ARM = <div className={style.leftArm}></div>;
+const RIGHT_LEG = <div className={style.leftArm}></div>;
+const LEFT_LEG = <div className={style.leftArm}></div>;
 function HangmanDrawing() {
   return (
     <div className={style.container}>
       {HEAD}
       {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
       <div className={style.hangerMouth} />
       <div className={style.hangerWood} />
       <div className={style.hanger} />
